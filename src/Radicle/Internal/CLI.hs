ff18491742ce83ff966d4e3bf12ed08ae281007f
Luke Palmer <lrpalmer@gmail.com>
2018-09-17 13:33:02 -0700

    Fixed bug when using $HOME by default for config/cache paths

--- src/Radicle/Internal/CLI.hs
+++ src/Radicle/Internal/CLI.hs
@@ -1,20 +1,35 @@
-module Radicle.Internal.CLI where
+module Radicle.Internal.CLI 
+    ( getConfig
+    , getHistory
+    )
+where
 
 import           Protolude
+import           Prelude (fail)
 import           System.Environment (lookupEnv)
 
 -- | Uses XDG_CONFIG_HOME if available.
 getConfig :: IO FilePath
 getConfig = do
-    mCfgHome <- lookupEnv "XDG_CONFIG_HOME"
-    pure $ case mCfgHome of
-        Nothing      -> "$HOME/.config/rad/config.rad"
-        Just cfgHome -> cfgHome <> "/rad/config.rad"
+    let configPaths = [ lookupEnv "XDG_CONFIG_HOME"
+                      , (fmap.fmap) (<> "/.config") (lookupEnv "HOME")
+                      ]
+    mCfgHome <- firstMaybeM configPaths
+    case mCfgHome of
+        Nothing      -> fail "Can't find config path: Neither $XDG_CONFIG_HOME nor $HOME are set"
+        Just cfgHome -> pure $ cfgHome <> "/rad/config.rad"
 
 -- | Uses XDG_CACHE_HOME if available.
 getHistory :: IO FilePath
 getHistory = do
-    mCfgHome <- lookupEnv "XDG_CACHE_HOME"
-    pure $ case mCfgHome of
-        Nothing      -> "$HOME/.cache/rad/history"
-        Just cfgHome -> cfgHome <> "/rad/history"
+    let cachePaths = [ lookupEnv "XDG_CACHE_HOME"
+                     , (fmap.fmap) (<> "/.cache") (lookupEnv "HOME")
+                     ]
+    mCacheHome <- firstMaybeM cachePaths
+    case mCacheHome of
+        Nothing        -> fail "Can't find config path: Neither $XDG_CACHE_HOME nor $HOME are set"
+        Just cacheHome -> pure $ cacheHome <> "/rad/config.rad"
+
+
+firstMaybeM :: (Monad m) => [m (Maybe a)] -> m (Maybe a)
+firstMaybeM = foldM (\m x -> maybe x (pure . Just) m) Nothing
