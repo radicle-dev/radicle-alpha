99cde9b43834d6c27c00071554f9618d765d831a
Luke Palmer <lrpalmer@gmail.com>
2018-09-18 08:21:18 -0700

    Fix XDG fallback using getXdgDirectory, much simple.

--- src/Radicle/Internal/CLI.hs
+++ src/Radicle/Internal/CLI.hs
@@ -5,31 +5,12 @@ module Radicle.Internal.CLI
 where
 
 import           Protolude
-import           Prelude (fail)
-import           System.Environment (lookupEnv)
+import           System.Directory (XdgDirectory(..), getXdgDirectory)
 
 -- | Uses XDG_CONFIG_HOME if available.
 getConfig :: IO FilePath
-getConfig = do
-    let configPaths = [ lookupEnv "XDG_CONFIG_HOME"
-                      , (fmap.fmap) (<> "/.config") (lookupEnv "HOME")
-                      ]
-    mCfgHome <- firstMaybeM configPaths
-    case mCfgHome of
-        Nothing      -> fail "Can't find config path: Neither $XDG_CONFIG_HOME nor $HOME are set"
-        Just cfgHome -> pure $ cfgHome <> "/rad/config.rad"
+getConfig = getXdgDirectory XdgConfig "rad/config.rad"
 
 -- | Uses XDG_CACHE_HOME if available.
 getHistory :: IO FilePath
-getHistory = do
-    let cachePaths = [ lookupEnv "XDG_CACHE_HOME"
-                     , (fmap.fmap) (<> "/.cache") (lookupEnv "HOME")
-                     ]
-    mCacheHome <- firstMaybeM cachePaths
-    case mCacheHome of
-        Nothing        -> fail "Can't find config path: Neither $XDG_CACHE_HOME nor $HOME are set"
-        Just cacheHome -> pure $ cacheHome <> "/rad/config.rad"
-
-
-firstMaybeM :: (Monad m) => [m (Maybe a)] -> m (Maybe a)
-firstMaybeM = foldM (\m x -> maybe x (pure . Just) m) Nothing
+getHistory = getXdgDirectory XdgData "rad/config.rad"
