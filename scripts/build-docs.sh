6958b9b1aa5b68eb4315bd896a08cc4de313e497
Julian K. Arni <julian@monadic.xyz>
2018-10-12 15:41:22 +0200

    wip

--- scripts/build-docs.sh
+++ scripts/build-docs.sh
@@ -1,8 +1,59 @@
-#!/bin/bash -
+#!/usr/bin/env bash
 
 set -o nounset
 set -o errexit
 
-cd docs
-pip install -r requirements.txt
-make html
+DEPLOY=false
+BUCKET="gs://docs.radicle.xyz"
+
+help(){
+    echo "./build-docs.sh [-d|-h]" 
+    echo " Builds the radicle documentation"
+    echo " Options:"
+    echo "   -d : Also deploy the documentation"
+    echo "   -h : Show this help text"
+    exit 0
+}
+
+# Helper to prevent pushd/popd from printing stuff to the console
+pushd () {
+    command pushd "$@" > /dev/null
+}
+
+popd () {
+    command popd "$@" > /dev/null
+}
+
+build(){
+    pushd "$PWD"/docs
+    [ -z "$NIX_PATH" ] && pip install -r requirements.txt
+    make html
+    popd
+}
+
+deploy(){
+    pushd "$PWD"/docs
+    gsutil -m cp -r build/html "$BUCKET"
+    popd
+}
+
+while getopts ":d:h" opt; do
+  case $opt in
+    h) 
+      help 
+      ;;
+    d)
+      DEPLOY=true
+      ;;
+    \?) 
+      echo "Invalid option: -$OPTARG" 
+      help
+      ;; 
+  esac 
+done
+
+build
+
+if [ "$DEPLOY" = true ] ; then
+    deploy
+fi
