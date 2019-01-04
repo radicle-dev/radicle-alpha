53619c9e3c6c2588a63b3738ef1a08ccbd7909eb
James Henri Haydon <james.haydon@gmail.com>
2019-01-04 08:54:31 +0000

    Very simple counter chain (with eval redef) benchmark.

--- exe/Benchmarks.hs
+++ exe/Benchmarks.hs
@@ -1,37 +1,33 @@
 module Benchmarks (main) where
 
-import Protolude
+import           Protolude
 
-import Criterion.Main
+import           Criterion.Main
 
-import Radicle
+import           Radicle
 
-steps :: [Text] -> IO Value
-steps is = snd <$> foldlM step (pureEnv, Number 0) is
+steps :: [Text] -> Value
+steps is = runIdentity $ snd <$> foldlM step (pureEnv, Number 0) is
   where
     step (s,_) t = do
       (res_, s') <- interpretWithState "[bench]" t s
       case res_ of
-        Left e -> panic (show e)
+        Left e  -> panic (show e)
         Right v -> pure (s', v)
 
-counter :: Int -> IO Value
-counter i = steps $
+counter :: Int -> Rational
+counter i = unwrap $ steps $
   [ "(def i (ref 0))"
   , "(def add (fn [x] (write-ref i (+ (read-ref i) x))))"
   , "(def eval (fn [expr state] (base-eval (list 'add expr) state)))" ]
   ++ (show <$> take i (cycle [1..20::Int]))
+  where
+    unwrap (Number r) = r
+    unwrap _          = panic "expecting a number result!"
 
--- Our benchmark harness.
 main :: IO ()
-main = do
-  v <- counter 100
-  print v
-  defaultMain [
-    bgroup "counter" [ bench "10"  $ whnf counter 10
-                   , bench "50"  $ whnf counter 50
-                   , bench "1000"  $ whnf counter 1000
+main = defaultMain [
+  bgroup "counter" [ bench "1000"  $ whnf counter 1000
                    , bench "10000" $ whnf counter 10000
-                   , bench "100000" $ whnf counter 100000
                    ]
     ]
