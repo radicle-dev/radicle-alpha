9ed855437f1e689c8b06ab02546c7bb973bb952e
Julian K. Arni <julian@monadic.xyz>
2019-01-15 19:37:17 +0100

    wip

--- nix/binary-varint.nix
+++ nix/binary-varint.nix
@@ -2,11 +2,17 @@
 mkDerivation {
   pname = "binary-varint";
   version = "0.1.0.0";
-  src = fetchgit {
+  src = (fetchgit {
     url = "http://github.com/oscoin/ipfs";
-    sha256 = "1pjf2hbp5qyda725wbra8yrjyzv6v0dnl0iw9idqq04jicz1x313";
+    sha256 = "0vy5a4h5bpyvn6i59gj0h94s9nmwyn3qgy3bs09f252axfnc3vpk";
     rev = "5627befa49dc756a690455f5bc4ee2993a0859eb";
-  };
+    postFetch = ''
+      cd $out
+      for l in binary-varint ipld-cid multibase multihash-cryptonite; do
+        rm "$l"/LICENSE
+        cp LICENSE "$l"/LICENSE
+      done'';
+  } + "/binary-varint") ;
   libraryHaskellDepends = [ base binary ];
   homepage = "https://github.com/oscoin/ipfs";
   description = "VarInt encoding/decoding via Data.Binary";
--- nix/ipld-cid.nix
+++ nix/ipld-cid.nix
@@ -5,11 +5,17 @@
 mkDerivation {
   pname = "ipld-cid";
   version = "0.1.0.0";
-  src = fetchgit {
+  src = (fetchgit {
     url = "http://github.com/oscoin/ipfs";
-    sha256 = "1pjf2hbp5qyda725wbra8yrjyzv6v0dnl0iw9idqq04jicz1x313";
+    sha256 = "0vy5a4h5bpyvn6i59gj0h94s9nmwyn3qgy3bs09f252axfnc3vpk";
     rev = "5627befa49dc756a690455f5bc4ee2993a0859eb";
-  };
+    postFetch = ''
+      cd $out
+      for l in binary-varint ipld-cid multibase multihash-cryptonite; do
+        rm "$l"/LICENSE
+        cp LICENSE "$l"/LICENSE
+      done'';
+  } + "/ipld-cid") ;
   libraryHaskellDepends = [
     base binary binary-varint bytestring cryptonite deepseq hashable
     multibase multihash-cryptonite text
