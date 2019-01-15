581661a16b156a056de2919901acf29aa987813f
James Henri Haydon <james.haydon@gmail.com>
2019-01-15 17:13:29 +0000

    Pure JSON API.

--- exe/Daemon.hs
+++ exe/Daemon.hs
@@ -1,18 +1,38 @@
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+
 module Daemon (main) where
 
 import           Protolude hiding (fromStrict, option)
 
-import           Servant
+import qualified Data.Aeson as A
 import           Network.Wai.Handler.Warp (run)
+import           Servant
 
 import           Server.Common
 
 import           Radicle
-import           Radicle.Internal.MachineBackend.EvalServer
 import           Radicle.Internal.MachineBackend.Ipfs
 
-type Query = "query" :> ReqBody '[PlainText] Value  :> Post '[PlainText] Value
-type Send  = "send"  :> ReqBody '[PlainText] Values :> Post '[JSON] ()
+newtype Expression = Expression { expression :: Value }
+  deriving (Generic)
+
+instance A.FromJSON Expression where
+  parseJSON = notImplemented
+
+instance A.ToJSON Expression where
+  toJSON = notImplemented
+
+newtype Expressions = Expressions { expressions :: [Value] }
+  deriving (Generic)
+
+instance A.FromJSON Expressions where
+  parseJSON = notImplemented
+
+instance A.ToJSON Expressions where
+  toJSON = notImplemented
+
+type Query = "query" :> ReqBody '[JSON] Expression  :> Post '[JSON] Expression
+type Send  = "send"  :> ReqBody '[JSON] Expressions :> Post '[JSON] ()
 type New   = "new"   :> Post '[JSON] IpnsId
 
 type DaemonApi =
@@ -35,5 +55,5 @@ server = machineEndpoints :<|> newMachine
   where
     newMachine :: Handler IpnsId = pure "thisisanIPNSid" -- TODO(james)
     machineEndpoints id = query id :<|> send id
-    query id v = pure $ List [String id, v] -- TODO(james)
-    send _id _vs = pure () -- TODO(james)
+    query id (Expression v) = pure $ Expression $ List [String id, v] -- TODO(james)
+    send _id (Expressions _vs) = pure () -- TODO(james)
