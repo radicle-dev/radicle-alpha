60e76895c358d27e3113cfb1060e1f45ee126279
Merle Breitkreuz <merle@monadic.xyz>
2019-02-18 12:00:02 +0100

    Undo DiffAppTest changes for debugging

--- test/e2e/DiffAppTest.hs
+++ test/e2e/DiffAppTest.hs
@@ -14,19 +14,15 @@ test_diff_propose = testCaseSteps "diff propose" $ \step -> do
     _ <- runTestCommand "git" ["config", "--global", "user.name", "Alice"]
     _ <- runTestCommand "git" ["config", "--global", "user.email", "alice@example.com"]
     _ <- runTestCommand' "rad-project" ["init"] ["project-name", "project desc", "1"]
-    _ <- runTestCommand "touch" ["test"]
-    _ <- runTestCommand "git" ["add", "test"]
-    _ <- runTestCommand "git" ["commit", "--message", "first commit"]
+    _ <- runTestCommand "git" ["commit", "--allow-empty", "--message", "first commit"]
     commitSha <- runTestCommand "git" ["rev-parse", "--short", "HEAD"]
 
     step "rad diff propose"
     _ <- runTestCommand "rad-diff" ["propose", "HEAD"]
 
     listOutput <- runTestCommand "rad-diff" ["list"]
-    assertContains listOutput $ "state      #"
-    assertContains listOutput $ "pending    0"
+    assertContains listOutput $ "(pending) [Alice] " <> commitSha <> " - first commit | DIFF 0"
 
     showOutput <- runTestCommand "rad-diff" ["show", "0"]
-    assertContains showOutput $ "pending 0"
-    assertContains showOutput $ "From " <> commitSha
+    assertContains showOutput $ "(pending) [Alice] " <> commitSha <> " - first commit | DIFF 0"
     assertContains showOutput $ "Comments\n---"
