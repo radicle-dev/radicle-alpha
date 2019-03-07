556645bfbc8d857bc086e855ec8750cd6fd0ae6d
Merle Breitkreuz <breitkreuz.m@googlemail.com>
2019-03-07 18:46:51 +0100

    Rename rad diff to rad patch (#522)
    * Rename rad diff to rad patch
    
    * Delete diff.rad, update patch.rad.

--- test/e2e/PatchAppTest.hs
+++ test/e2e/PatchAppTest.hs
@@ -1,14 +1,14 @@
--- | Test the Radicle diff app.
-module DiffAppTest
-    ( test_diff_propose
+-- | Test the Radicle patch app.
+module PatchAppTest
+    ( test_patch_propose
     ) where
 
 import           Protolude
 
 import           Test.E2ESupport
 
-test_diff_propose :: TestTree
-test_diff_propose = testCaseSteps "diff propose" $ \step -> do
+test_patch_propose :: TestTree
+test_patch_propose = testCaseSteps "patch propose" $ \step -> do
     step "Init project"
     _ <- runTestCommand "rad-key" ["create"]
     _ <- runTestCommand "git" ["config", "--global", "user.name", "Alice"]
@@ -20,23 +20,23 @@ test_diff_propose = testCaseSteps "diff propose" $ \step -> do
     _ <- runTestCommand "git" ["commit", "--message", "first commit"]
     commitSha <- runTestCommand "git" ["rev-parse", "--short", "HEAD"]
 
-    step "rad diff propose"
-    _ <- runTestCommand "rad-diff" ["propose", "HEAD"]
+    step "rad patch propose"
+    _ <- runTestCommand "rad-patch" ["propose", "HEAD"]
 
-    listOutput <- runTestCommand "rad-diff" ["list"]
+    listOutput <- runTestCommand "rad-patch" ["list"]
     assertContains listOutput "state      #"
     assertContains listOutput "pending    0"
 
-    showOutput <- runTestCommand "rad-diff" ["show", "0"]
+    showOutput <- runTestCommand "rad-patch" ["show", "0"]
     assertContains showOutput "pending 0"
     assertContains showOutput $ "From " <> commitSha
     assertContains showOutput "Comments\n---"
 
-    step "rad diff accept"
+    step "rad patch accept"
     _ <- runTestCommand "git" ["checkout", "master"]
-    _ <- runTestCommand "rad-diff" ["accept", "0"]
+    _ <- runTestCommand "rad-patch" ["accept", "0"]
 
-    showAcceptedOutput <- runTestCommand "rad-diff" ["show", "0"]
+    showAcceptedOutput <- runTestCommand "rad-patch" ["show", "0"]
     assertContains showAcceptedOutput "accepted 0"
 
     logOutput <- runTestCommand "git" ["log", "master"]
