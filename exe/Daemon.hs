6714aa413562fa3e67bdefaf4d57f6d2560f4374
Thomas Scholtes <thomas@monadic.xyz>
2019-03-13 11:03:22 +0100

    Don’t load failed machines over and over
    Before we tried to reload machines that failed to initialize every ten
    seconds. This included invalid machines that failed to evaluate
    properly. We disable this behavior until we have a better strategy to
    catch errors and have exponential back-off.

--- exe/Daemon.hs
+++ exe/Daemon.hs
@@ -472,10 +472,8 @@ poll = traverseMachines pollMachine
           logDebug "Polling.." [("machine-id", getMachineId machineId)]
           refreshAsReader machineId >> pure ()
         modifyMachine machineId $ \m' -> pure (m' { machinePolling = newPoll }, ())
-      UninitialisedReader t -> do
-        delta <- liftIO $ timeSince t
-        when (delta > lowFreqPollPeriod) $ initReaderNoFail id $> ()
-      _ -> pure () -- Writers don't require any polling.
+      -- Don't update owned and unavailable machines.
+      _ -> pure ()
 
 -- | Returns the amount of time since the last time the machine was
 -- updated.
