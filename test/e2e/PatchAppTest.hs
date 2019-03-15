f32df2395164fc5b70a9a0a2d7671f7926c692f3
Merle Breitkreuz <breitkreuz.m@googlemail.com>
2019-03-15 11:42:49 +0100

    Cleanup patch app e2e tests (#567)

--- test/e2e/PatchAppTest.hs
+++ test/e2e/PatchAppTest.hs
@@ -1,24 +1,28 @@
 -- | Test the Radicle patch app.
 module PatchAppTest
     ( test_patch_propose
+    , test_patch_accept
     ) where
 
 import           Protolude
 
 import           Test.E2ESupport
 
-test_patch_propose :: TestTree
-test_patch_propose = testCaseSteps "patch propose" $ \step -> do
+initProjectWithCommit :: TestM Text
+initProjectWithCommit = do
     prepareRadicle
-    step "Init project"
     _ <- runTestCommand' "rad-project" ["init"] ["project-name", "project desc", "1"]
     _ <- runTestCommand "git" ["checkout", "-b", "f/test"]
     _ <- runTestCommand "touch" ["test"]
     _ <- runTestCommand "git" ["add", "test"]
-    _ <- runTestCommand "git" ["commit", "--message", "first commit"]
-    commitSha <- runTestCommand "git" ["rev-parse", "--short", "HEAD"]
+    runTestCommand "git" ["commit", "--message", "first commit"]
+
+test_patch_propose :: TestTree
+test_patch_propose = testCaseSteps "patch propose" $ \step -> do
+    initProjectWithCommit
 
     step "rad patch propose"
+    commitSha <- runTestCommand "git" ["rev-parse", "--short", "HEAD"]
     _ <- runTestCommand "rad-patch" ["propose", "HEAD"]
 
     listOutput <- runTestCommand "rad-patch" ["list"]
@@ -29,6 +33,13 @@ test_patch_propose = testCaseSteps "patch propose" $ \step -> do
     assertContains showOutput "pending 0"
     assertContains showOutput $ "From " <> commitSha
     assertContains showOutput "Comments\n---"
+
+test_patch_accept :: TestTree
+test_patch_accept = testCaseSteps "patch accept" $ \step -> do
+    initProjectWithCommit
+
+    step "rad patch propose"
+    _ <- runTestCommand "rad-patch" ["propose", "HEAD"]
 
     step "rad patch accept"
     _ <- runTestCommand "git" ["checkout", "master"]
