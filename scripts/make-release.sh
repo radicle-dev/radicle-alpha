189baaaa4619fdb1cec27f1dc35e3a2aee4a0152
Julian K. Arni <julian@monadic.xyz>
2019-04-09 11:32:39 +0200

    Actually commit in release script.
    And fix sundry release stuff.

--- scripts/make-release.sh
+++ scripts/make-release.sh
@@ -2,9 +2,12 @@
 
 VERSION=$1
 
+set -o errexit
+
 if ! [[ "$VERSION" =~ ^[0-9]+\.[0-9]+\.[0-9]+ ]]; then
     echo "Bad version: $VERSION"
     echo "Should have format 'x.y.z', where x, y and z are numbers"
+    exit 1
 fi
 
 if [ "$(git status --untracked-files=no --porcelain)" != " M ChangeLog.md" ]; then
@@ -15,10 +18,16 @@ fi
 
 # Update package.yaml
 mv package.yaml old-package.yaml
-sed "s/^version: '.*'$/version: '$VERSION'" old-package.yaml > package.yaml
+sed "s/^version: '.*'$/version: '$VERSION'/" old-package.yaml > package.yaml
 rm old-package.yaml
 
-echo "git add package.yaml"
-echo "git add ChangeLog.md"
-echo "git commit -m \"Version update: $VERSION\""
-echo "git tag v$VERSION"
+# Update daemon-api.yaml
+mv docs/source/daemon-api.yaml old-daemon-api.yaml
+sed "s/^  version: .*$/  version: $VERSION/" old-daemon-api.yaml > docs/source/daemon-api.yaml
+rm old-daemon-api.yaml
+
+git add package.yaml
+git add ChangeLog.md
+git add docs/source/daemon-api.yaml
+git commit -m "Version update: $VERSION"
+git tag "v$VERSION"
